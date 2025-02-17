2ce8b21361b273c42daa68fa77e347aab5139842
zebm <marklandzeb@gmail.com>
2025-02-18 10:54:54 +1300

    Adding helper methods to Scanner and intial cases for tokens

--- Scanner.java
+++ Scanner.java
@@ -10,6 +10,9 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
@@ -23,5 +26,64 @@ public class Scanner {
 
         tokens.add(new Token(EOF, "", null, line))
         return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG):
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=')? GREATER_EQUAL  : GREATER);
+                break;
+
+            default:
+                // need to also report character
+                // need to look into logging errors and printing all found
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        tokens.add(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
