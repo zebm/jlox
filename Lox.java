584e4b1cd0d100be5941b03002b6a7466a96b601
zebm <marklandzeb@gmail.com>
2025-02-17 21:31:11 +1300

    Created main Lox class and added intial methods to start building scanner

---
+++ Lox.java
@@ -0,0 +1,62 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            System.out.println("Usage: lox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+
+    }
+
+    /**
+     * Runs the interpreter given a source file
+     * @param path to input file
+      * @throws 
+     */
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    /**
+     *  Interactive prompt function, runs uuser input until EOF
+     */
+    private static void runPrompt() {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    /**
+     * Runs the interpreter given a string of source code
+      * @param source code to be interpreted
+      */
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now just print tokens
+        for (Token token : tokens){
+            System.out.println(token);
+         }
+    }
+}
