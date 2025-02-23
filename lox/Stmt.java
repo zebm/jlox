358cd285c76ab8265766bb447551ea306d3fe88a
zebm <marklandzeb@gmail.com>
2025-02-23 21:31:13 +1300

    Ammended GenerateAst to implement variable syntax rules

--- lox/Stmt.java
+++ lox/Stmt.java
@@ -1,9 +1,12 @@
 package jlox.lox;
 
+import java.util.List;
+
 abstract class Stmt {
    interface Visitor<R> {
         R visitExpressionStmt(Expression stmt);
         R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
     }
     static class Expression extends Stmt {
         Expression(Expr expression) {
@@ -29,6 +32,21 @@ abstract class Stmt {
         }
 
         final Expr expression;
+    }
+
+    static class Var extends Stmt {
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
+        }
+
+        final Token name;
+        final Expr initializer;
     }
 
 
