19e5832c6505c0ad23483b7dd20f26142f5339ee
zebm <marklandzeb@gmail.com>
2025-02-25 10:27:02 +1300

    Implemented if statements into Parser and Interpreter

--- lox/Parser.java
+++ lox/Parser.java
@@ -44,14 +44,37 @@ class Parser {
     *  Implements the statment grammar rule for the parser: 
     * 
     *  statement -> exprStmt
-    *              | printStmt ;
+    *              | ifStmt
+    *              | printStmt 
+    *              | block ;
     *
     */
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    /**
+     * Implements the if statement rule for the parser:
+     * 
+     * ifStmt -> "if" "(" expression ")" statement
+     *            ( "else" statement )? ;
+     */
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after if.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     /** 
