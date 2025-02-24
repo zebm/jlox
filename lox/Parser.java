a7504cde806e654d21c44419cf470905c71ac557
zebm <marklandzeb@gmail.com>
2025-02-25 08:35:24 +1300

    Implemented block grammar into Parser

--- lox/Parser.java
+++ lox/Parser.java
@@ -49,6 +49,7 @@ class Parser {
     */
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
     }
@@ -92,6 +93,22 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+   }
+
+   /**
+    * Implements the block grammar rule for the parser:
+    * 
+    * block -> "{" declaration* "}" ;
+    */
+   private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
    }
 
      /** 
