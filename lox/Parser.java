6489d2010918b57be0c12365c84e4aa79b8728c9
zebm <marklandzeb@gmail.com>
2025-03-11 10:12:59 +1300

    Implemented for loops into the parser.

--- lox/Parser.java
+++ lox/Parser.java
@@ -1,6 +1,7 @@
 package jlox.lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static jlox.lox.TokenType.*;
 
@@ -50,12 +51,62 @@ class Parser {
     *
     */
     private Stmt statement() {
+        if (match(FOR)) return forStatement();
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(WHILE)) return whileStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    /**
+     * Implements the for statement rule for the parser:
+     * 
+     * forStmt -> "for" "(" ( varDecl | exprStmt | ";" )
+     *            expression? ";"
+     *            expression? ")" statement ;
+     */
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+        Stmt initializer;
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ':' after loop condition.");
+
+        Expr increment = null;
+        if (!check(RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ') after for clauses.");
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(
+                Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)));
+        }
+
+        if (condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
     }
 
     /**
